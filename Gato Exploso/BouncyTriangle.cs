949516f2c6fae807956a1df6c48b0411329badce
joshyscrib <122238197+joshyscrib@users.noreply.github.com>
2023-12-08 16:05:25 -0800

    Create BouncyTriangle.cs

---
+++ Gato Exploso/BouncyTriangle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Gato_Exploso
+{
+  class BouncyTriangle
+  {
+  int x;
+  int y;
+  int width;
+  int height;
+  int health;
+  bool good;
+    public int attack(){
+      return 2 * (Math.Random() * 10);
+    }
+    
+    
+  }
+
+}
