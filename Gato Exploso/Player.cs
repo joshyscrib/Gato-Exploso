3a995a64bb0e749e49d28f4f77a04cb22316dbaf
joshyscrib <122238197+joshyscrib@users.noreply.github.com>
2023-12-08 16:58:53 -0800

    Player/Tile
    Added player class and tile class

---
+++ Gato Exploso/Player.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Net.Mime;
+
+
+namespace Gato_Exploso
+{
+    internal class Player
+    {
+        int x = 0;
+        int y = 0;
+        int speed = 5;
+
+        public void Move(KeyboardState keyState)
+        {
+            if (keyState.IsKeyDown(Keys.W))
+            {
+                y -= speed;
+            }
+            if (keyState.IsKeyDown(Keys.A))
+            {
+                x -= speed;
+            }
+            if (keyState.IsKeyDown(Keys.S))
+            {
+                y += speed;
+            }
+            if (keyState.IsKeyDown(Keys.D))
+            {
+                x += speed;
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+    }
+}
---
+++ Gato Exploso/Tile.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Net.Mime;
+
+namespace Gato_Exploso
+{
+    internal abstract class Tile
+    {
+        protected Texture2D tileTexture;
+        public abstract void Load();
+        public abstract void Draw(SpriteBatch spriteBatch, int x, int y);
+
+    }
+}
