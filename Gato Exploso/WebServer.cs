c9e91546149e0e8d94bd3e8ee0e239b1c939eb62
joshyscrib <122238197+joshyscrib@users.noreply.github.com>
2024-01-03 19:30:25 -0800

    Web server
    began to make a web server class

---
+++ Gato Exploso/WebServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gato_Exploso
+{
+    internal class WebServer
+    {
+        HttpListener listener = new HttpListener();
+        public void Start()
+        {
+            
+            // Add the prefixes.
+                
+                listener.Prefixes.Add("http://127.0.0.1:80");
+            
+            listener.Start();
+            Console.WriteLine("Listening...");
+        }
+
+    }
+}
