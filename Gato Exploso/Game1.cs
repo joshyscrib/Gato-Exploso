96516f9b77e8188e0957b966d4bb0dbbf56042c2
joshyscrib <122238197+joshyscrib@users.noreply.github.com>
2024-05-14 09:58:48 -0700

    mine

--- Gato Exploso/Game1.cs
+++ Gato Exploso/Game1.cs
@@ -956,7 +956,7 @@ namespace Gato_Exploso
                 {
                     if(GetTileAt((int)curMob.x / 32,(int)curMob.y / 32).GetTileObjects()[0].GetType() == typeof(LandMine))
                     {
-                
+                        GetTileAt((int)curMob.x / 32,(int)curMob.y / 32).StartExplosion();
                     }
                 }
 
